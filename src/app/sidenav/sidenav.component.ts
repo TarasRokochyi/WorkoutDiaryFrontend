ed22c3fa70a825b136cb5d1e9e464a1ec4b63484
TarasRokochyi <tarrok2005@gmail.com>
2026-06-04 02:33:03 +0300

    changed style of sidenav

--- src/app/sidenav/sidenav.component.ts
+++ src/app/sidenav/sidenav.component.ts
@@ -1,5 +1,7 @@
-import { Component, Input, Output, EventEmitter, HostBinding } from '@angular/core';
+import { Component, HostBinding, OnInit } from '@angular/core';
 import { SidenavService } from './sidenav.service';
+import { UserService } from '../shared/services/user.service';
+import { User } from '../_interfaces/user.model';
 
 @Component({
   selector: 'app-sidenav',
@@ -7,9 +9,14 @@ import { SidenavService } from './sidenav.service';
   templateUrl: './sidenav.component.html',
   styleUrl: './sidenav.component.css'
 })
-export class SidenavComponent {
+export class SidenavComponent implements OnInit {
+  user?: User;
 
-  constructor(public sidenavService: SidenavService) {}
+  constructor(public sidenavService: SidenavService, private userService: UserService) {}
+
+  ngOnInit(): void {
+    this.userService.getCurrentUser().subscribe(user => this.user = user);
+  }
 
   @HostBinding('class.is-expanded')
   get isExpanded() {
